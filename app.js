8881fd34a20d8f6ff1380263e7eb64a9557c5b65
blancarevilla <blancaindira1@gmail.com>
2022-04-24 14:02:28 -0400

    Subienod archivos de registro, login y app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(4040, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
