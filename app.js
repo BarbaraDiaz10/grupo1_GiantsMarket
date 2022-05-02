c5f52a38849dae2a6891d06ad14b940b75d56407
irojof <irojo.f@gmail.com>
2022-05-02 19:33:36 -0400

    se agrega index, algunos estilos e imagenes

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ app.use(express.static('public'));
 
 
 app.listen(4040, ()=>{
-    console.log('Servidor funcionando');
+    console.log('Servidor funcionando. Puerto:4040');
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/detalleproducto', (req,res)=>{
+    res.sendFile(__dirname + '/views/detalleproducto.html');
 });
