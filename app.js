e5c7e35bbb336d813438de68bea38b6cc5701c9a
irojof <irojo.f@gmail.com>
2022-05-04 23:03:57 -0400

    correccion header y footer. adicion product detail. adicion estilos

--- app.js
+++ app.js
@@ -10,12 +10,21 @@ app.listen(4040, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/index.html');
 });
+app.get('/index', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
-app.get('/detalleproducto', (req,res)=>{
-    res.sendFile(__dirname + '/views/detalleproducto.html');
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+app.get('/productCart', (req,res)=>{
+    res.sendFile(__dirname + '/views/productCart.html');
 });
