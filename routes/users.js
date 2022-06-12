93b34060cccfdc8f5dc240fe9a320e7d293cd11a
blancarevilla <blancaindira1@gmail.com>
2022-06-12 14:53:13 -0400

    modificando users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+const mainController = require ('../controllers/mainController')
+const controller = require ('../controllers/productsController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/', controller.index);
+router.get('/login', mainController.login);
+router.get('/register', mainController.register);
 
 module.exports = router;
