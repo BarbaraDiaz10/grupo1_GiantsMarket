1dce10d9dd2c6004a8d29cd1c278fcf5ab7bea78
blancarevilla <blancaindira1@gmail.com>
2022-06-26 15:38:33 -0400

    Subiendo cambios de register

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 const { validationResult } = require('express-validator');
 
 const userController = {
     index: (req, res) => {
+        
+        
         res.render("index", { products })
     },
 
@@ -15,6 +19,30 @@ const userController = {
 
     register: (req, res) => {
         res.render("register")
+
+    },
+
+    store: (req, res) => {
+        /***********************PARA LAS IMAGENES ***********************/
+        let image
+   
+
+        if (req.files[0] != undefined) {
+            image = req.files[0].filename
+        } else {
+            image = "default-image.png"
+        }
+        /********************************************************** */
+       
+        let newUsers = {
+           
+            id: users[users.length - 1].id + 1,
+            ...req.body,
+            image: image //PARA LAS IMAGENES
+        }
+        users.push(newUsers);
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ''));
+        res.redirect('/');
 
     },
     user: (req, res) => {
@@ -46,6 +74,7 @@ const userController = {
         req.session.form = {}
         req.session.form.email = req.body.email;
         req.session.form.password = req.body.password;
+
         res.redirect("/");
     }
 }
