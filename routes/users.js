66c068ca2e25339247bce68c7a602ec245a4f708
blancarevilla <blancaindira1@gmail.com>
2022-06-26 15:39:49 -0400

    subiendo cambios de registro

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
-const path = require('path');
+const multer = require('multer');
 const mainController = require('../controllers/userController')
 const validationRegister = require('../middlewares/register');
 const validationLogin = require('../middlewares/login');
+const path = require('path'); 
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/images/users')
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+
+const upload = multer({ storage: storage })
+
 /* GET users listing. */
 router.get('/', mainController.index);
 router.get('/login', mainController.login);
 router.post('/login', validationLogin, mainController.userLogin);
 router.get('/register', mainController.register);
-router.post('/register', validationRegister, mainController.user);
+router.post('/register', upload.any(), validationRegister, mainController.store);
 module.exports = router;
