742baa2e3e13c96c8788623a4c83caf323c31451
BarbaraDiaz10 <barbarita.n10@gmail.com>
2022-06-26 13:54:38 -0400

    se agrega una validación mas para el email del login

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,7 +1,8 @@
 const { body } = require('express-validator');
 
 const validationLogin = [
-    body('email').notEmpty().withMessage("El email no puede ser vacio"),
+    body('email').notEmpty().withMessage("El email no puede ser vacio").bail()
+    .isEmail().withMessage("Debe ingresar un formato de email válido"), ,
     body('password').notEmpty().withMessage("La contraseña no puede ser vacio").bail()
     .isLength({ min: 8, max: 10 }).withMessage("La contraseña debe tener entre 8 a 10 caracteres")
 
