b73ad2e4b6367efbebaff91508d8b2186abb94b4
blancarevilla <blancaindira1@gmail.com>
2022-07-28 19:39:05 -0400

    Cambiando nombres de campo Id

--- database/scripts/tables.sql
+++ database/scripts/tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE roles (
 
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
-`category_id` INT(10) PRIMARY KEY NOT NULL,
+`id` INT(10) PRIMARY KEY NOT NULL,
 `category_name` varchar(50) NOT NULL);
 
 
@@ -29,7 +29,7 @@ foreign key (`role_id`) references `roles` (`role_id`)
 
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
-`product_id` INT(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+`id` INT(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 `name` varchar(50) NOT NULL,
 `description` text NOT NULL,
 `description_detail` text NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE `products` (
 `shipping` int(10) NOT NULL,
 `stock` int(10) NOT NULL,
 
-foreign key (`category_id`) references `category` (`category_id`)
+foreign key (`id`) references `category` (`id`)
 );
 
 DROP TABLE IF EXISTS `product_user`;
@@ -50,5 +50,5 @@ CREATE TABLE `product_user` (
 `user_id` INT(10) NOT NULL,
 `product_id` INT(10) NOT NULL,
 foreign key (`user_id`) references `users` (`user_id`),
-foreign key (`product_id`) references `products` (`product_id`)
+foreign key (`product_id`) references `products` (`id`)
 );
