792f7302cedf642400bee418acb458e3c3408c69
blancarevilla <blancaindira1@gmail.com>
2022-08-14 13:23:03 -0400

    Validacion en Back register user, login y product

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,10 +1,41 @@
-const { body } = require('express-validator');
+const { check, body } = require('express-validator');
+//const {users} = require("../data/db");
+const bcrypt = require('bcryptjs')
+const db = require('../database/models')
 
 const validationLogin = [
-    body('email').notEmpty().withMessage("El email no puede estar vacio").bail()
+    check('email')
+    .isEmail()
+    .withMessage('Debes ingresar un email válido'),
+  
+    body('email').custom(value => {
+    
+      return db.User.findOne({
+          where : {
+              email : value
+          }
+      })
+      .then(user => {
+          if(!user){
+              return Promise.reject('Este email no está registrado')
+          }
+      })
+  }),
+  body('password').custom((value, {req}) => {
+  return db.User.findOne({where: { email: req.body.email}})
+  .then((user)=>{
+    if (!bcrypt.compareSync(value, user.password)){
+      return Promise.reject()
+    }
+  }).catch(() => {
+    return Promise.reject("Las contraseñas no coinciden");
+  });
+  })
+
+    /*body('email').notEmpty().withMessage("El email no puede estar vacio").bail()
     .isEmail().withMessage("Debe ingresar un formato de email válido"),
     // body('password').notEmpty().withMessage("La contraseña no puede ser vacio").bail()
-    // .isLength({ min: 8, max: 10 }).withMessage("La contraseña debe tener entre 8 a 10 caracteres")
+    // .isLength({ min: 8, max: 10 }).withMessage("La contraseña debe tener entre 8 a 10 caracteres")*/
 
 ]
 
--- middlewares/register.js
+++ middlewares/register.js
@@ -1,7 +1,52 @@
-const { body } = require('express-validator');
+const { check, body } = require('express-validator');
+//const {users} = require("../data");
+const db = require("../database/models");
 
 const validationRegister = [
-    body('first_name').notEmpty().withMessage("El nombre no puede ser vacio"),
+    check('email').notEmpty().withMessage("El email no puede ser vacio").bail()
+    .isEmail().withMessage("Debe ingresar un formato de email válido"),
+    
+
+    body('email').custom(value => {
+        /* let user = users.filter(user =>{
+            return user.email == value
+        }) */
+        return db.User.findOne({
+            where : {
+                email : value
+            }
+        })
+        .then(user => {
+            if(user){
+                return Promise.reject('Este email ya está registrado')
+            }
+        })
+    }),
+
+    check('first_name')
+        .notEmpty().withMessage("El nombre no puede estar vacio").bail()
+        .withMessage("Debe tener al menos 2 caracteres"),
+        
+    check('last_name')
+        .notEmpty().withMessage("El apellido no puede estar vacio").bail()
+        .withMessage("Debe tener al menos 3 caracteres"),
+
+    check('password')
+        .notEmpty()
+        .withMessage('Debes ingresar una contraseña')
+        .isLength({
+            min: 8,
+            max: 10
+        })
+    .withMessage('La contraseña debe tener al menos 8 caracteres'),
+
+    check ('image')
+    .notEmpty().withMessage('Debe ser un archivo valido (JPG, JPEG, PNG, GIF)')
+
+]
+
+
+    /*body('first_name').notEmpty().withMessage("El nombre no puede ser vacio"),
     body('last_name').notEmpty().withMessage("El apellido no puede ser vacio"),
     body('email').notEmpty().withMessage("El email no puede ser vacio").bail()
     .isEmail().withMessage("Debe ingresar un formato de email válido"),
@@ -11,6 +56,6 @@ const validationRegister = [
     body('role_id').notEmpty().withMessage("El rol no puede ser vacio")
 
 
-]
+]*/
 
 module.exports = validationRegister;
