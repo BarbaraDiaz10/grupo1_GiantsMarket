a1b4bf650d609a7a2658e06980706b6658350258
blancarevilla <blancaindira1@gmail.com>
2022-08-21 12:50:43 -0400

    llamando coleccion de api user

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 const methodOverride = require('method-override');
 var app = express();
 const auth = require('./middlewares/auth')
+const apiUsersRouter = require('./routes/api/users')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,6 +33,9 @@ app.use(auth)
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
+
+//Llamo a la colección de api
+app.use('/api/users',apiUsersRouter);
 
 
 // catch 404 and forward to error handler
